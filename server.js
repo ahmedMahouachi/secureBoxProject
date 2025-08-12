c9feb42993316a4c086dbc6f11b94dcc40afbd51
ahmedMahouachi <ahmedmahouachi66@gmail.com>
2025-08-12 10:59:03 +0100

    database connection

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
+require('dotenv').config();
+const { connectDb } = require("./database/db");
 
-
-
-app.listen(3000, ()=> {
-    console.log('Api on http://localhost:3000')
-})
+connectDb(process.env.MONGO_URI).then(() => {
+  app.listen(3000, () => {
+    console.log("Api on http://localhost:3000");
+  });
+});
