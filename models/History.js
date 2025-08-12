ee5b8840ec3b1b0d81c202ea774078b01df097ca
Abdallah B <m.bougjdi@hotmail.fr>
2025-08-12 16:19:04 +0200

    Modifs models

--- models/History.js
+++ models/History.js
@@ -6,23 +6,37 @@ const historySchema = new mongoose.Schema({
     ref: 'User',    // référence à la collection users du groupe A
     required: true,
   },
-  documentId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Document', // référence à la collection documents du groupe A
-    required: true,
-  },
+
+//   
+//     documentId: { 
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Document', // référence à la collection documents du groupe A
+//     required: true,
+//   },
   action: {
     type: String,
     required: true,
-    enum: ['create', 'update', 'delete'],  // actions principales sur documents
+    // actions principales sur documents
   },
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  route: {
+    type: String,
+    required: true,
+  },
+  method: {
+    type: String,
+    required: true,
+  },
+  adresseIP: {
+    type: String,
+    required: true,
   }
 });
 
-// Création du modèle History
+
 const History = mongoose.model('History', historySchema);
 
 module.exports = History;
