5103136cda2bc7bf79f4a78f173f70c8e582786e
Mehreen34 <mehreen.mans@gmail.com>
2025-08-12 12:18:19 +0200

    changement

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
-
 const app = express();
+
+const {connectDb} = require('./database/db')
 
 
 
 app.listen(3000, ()=> {
     console.log('Api on http://localhost:3000')
+})
+
+connectDb(process.env.MONGO_URI)
+.then(() => {
+    app.listen(3000, () => {
+        console.log("Api on http://localhost:3000");
+    })
 })
