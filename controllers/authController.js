ee7c328d4a52d58a81003650f72d82946c9d7537
hghania <hamzighania2@gmail.com>
2025-08-12 18:15:00 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,32 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.login = async (req, res) => {
-    res.send('login marche');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ succes: false, message: "user no trouvé" });
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    return res
+      .status(401)
+      .json({ succes: false, message: "mot de passe incorrect" });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "2h",
+    }
+  );
+  res.json({ succes: true, message: "vous etes connecté", token });
 };
+
 exports.register = async (req, res) => {
-    res.send('register marche');
+  res.send("register marche");
 };
