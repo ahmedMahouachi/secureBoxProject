714fc5f6f94243d72fe67a959c2bc7598e99ac55
Manon <vatresmanon17@gmail.com>
2025-08-13 15:46:19 +0200

    Add create and get with userId in token User

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,21 +1,21 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
 const {
     getHistory,
     getHistoryById,
     createHistoryById,
-    deleteHistory,
-    updateHistory
+    deleteHistory
 } = require("../controllers/adminController");
 
 const router = express.Router();
 
 //Route de l'historique
-router.get("/get_history/:userId", getHistory);
+router.get("/get_history/",authMiddleware, getHistory);
 router.get("/get_history_by_id/:historyId", getHistoryById);
 
-router.post("/create_history/:userId", createHistoryById);
+router.post("/create_history",authMiddleware, createHistoryById);
 router.delete("/delete_history/:historyId", deleteHistory);
-router.put("/update_history/:historyId", updateHistory);
+//router.put("/update_history/:historyId", updateHistory);
 
 
 module.exports = router;
