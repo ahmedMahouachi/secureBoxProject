f54e69f2cbfcdd50b4871b367c88929b1a062a85
bxicluna <80334075+bxicluna@users.noreply.github.com>
2025-08-13 17:18:52 +0200

    intégration du token

--- routes/documentroute.js
+++ routes/documentroute.js
@@ -2,24 +2,22 @@ const express = require("express");
 const router = express.Router();
 const documentController = require("../controllers/documentController");
 const upload = require('../middlewares/uploadMiddleware')
+const authMiddleware = require('../middlewares/authMiddleware')
 
 
 // Routes CRUD
 
 // POST /documents → créer un document (upload d'un fichier)
-router.post("/", upload.single("file"), documentController.createDocument);
+router.post("/",authMiddleware, upload.single("file"), documentController.createDocument);
 
 // GET /documents → récupérer tous les documents
-router.get("/", documentController.getDocuments);
+router.get("/",authMiddleware, documentController.getDocuments);
 
 // GET /documents/:id → récupérer un document par ID
-router.get("/:id", documentController.getDocumentById);
-
-// PUT /documents/:id → mettre à jour un document (avec potentiellement un nouveau fichier)
-router.put("/:id", upload.single("file"), documentController.updateDocument);
+router.get("/:id",authMiddleware, documentController.getDocumentById);
 
 // DELETE /documents/:id → supprimer un document
-router.delete("/:id", documentController.deleteDocument);
+router.delete("/:id",authMiddleware, documentController.deleteDocument);
 
 // PUT /documents/:id/rename → renommer un fichier 
 router.put("/:id/rename", documentController.renameDocument);
