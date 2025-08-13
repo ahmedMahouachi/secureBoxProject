eb0a06cf860239d6bea7c3261a316bd0033cbad2
Manon <vatresmanon17@gmail.com>
2025-08-13 12:18:43 +0200

    update Controler, add create, delete and upadate

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -39,13 +39,70 @@ const getHistoryById = async (req, res) => {
     const history = await History.findById(historyId);
 
     if (!history) {
-        return res.status(404).json({ message: "History not found" });
+        return res.status(404).json({ message: "Enregistrement non trouvé" });
     }
 
     return res.json(history);
 };
 
+const createHistoryById = async (req, res) => {
+    try {
+        const historyUserId = req.params.userId;
+        const historyRoute = req.body.route;
+        const historyAdresseIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress || "0.0.0.0";
+
+        const history = await History.create({
+            userId : historyUserId,
+            action : "creation",
+            route : historyRoute, 
+            method : "CREATE",
+            adresseIP : historyAdresseIP
+        });
+
+        res.status(201).json(history);
+
+    } catch (error) {
+        res.status(500).json({message: "une erreur est survenu", error : error.message});
+
+    }
+};
+
+const deleteHistory = async (req, res) =>{
+    const historyId = req.params.historyId;
+        const historyRoute = req.body.route;
+
+    const history = await History.findById(historyId);
+
+    if (!history) {
+        return res.status(404).json({ message: "Enregistrement non trouvé" });
+    }
+
+    await history.deleteOne();
+    res.status(204).send();
+}
+
+//A vérifié
+const updateHistory = async (req, res) => {
+    const historyId = req.params.historyId;
+    const historyRoute = req.body.route;
+
+    const history = await History.findById(historyId);
+    if (!history) {
+        return res.status(404).json({ message: "Enregistrement non trouvé" });
+    }
+    
+    const filter = {id: historyId};
+    const update = {route: historyRoute};
+
+    history = await History.findOneAndUpdate(filter, update);
+    
+    res.status(201).json(history);
+};
+
 module.exports = {
-getHistoryById,
-getHistory
+    getHistoryById,
+    getHistory,
+    createHistoryById,
+    deleteHistory,
+    updateHistory
 };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const {
     getHistory,
-    getHistoryById
+    getHistoryById,
+    createHistoryById,
+    deleteHistory,
+    updateHistory
 } = require("../controllers/adminController");
 
 const router = express.Router();
@@ -9,5 +12,10 @@ const router = express.Router();
 //Route de l'historique
 router.get("/get_history/:userId", getHistory);
 router.get("/get_history_by_id/:historyId", getHistoryById);
+
+router.post("/create_history/:userId", createHistoryById);
+router.delete("/delete_history/:historyId", deleteHistory);
+router.put("/update_history/:historyId", updateHistory);
+
 
 module.exports = router;
