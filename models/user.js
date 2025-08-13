015890fbdb786aba78c289b219357a90f0b23f46
hghania <hamzighania2@gmail.com>
2025-08-13 11:19:34 +0200

    ajout de middelware et reccuperation des données

--- models/user.js
+++ models/user.js
@@ -1,31 +1,31 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-        },
-        lastName: {
-            type: String,
-        },
-        email: {
-            type: String,
-            lowercase: true,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            select: false,
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ["client", "admin"],
-            default: "client",
-        },
+  {
+    firstName: {
+      type: String,
     },
-    { timestamps: true }
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["client", "admin"],
+      default: "client",
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
