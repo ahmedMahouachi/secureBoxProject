532522dd16a67b52c88df6f6b9a34f30cf865720
Manon <vatresmanon17@gmail.com>
2025-08-14 17:06:42 +0200

    add auditMiddleware documentroute

--- routes/documentroute.js
+++ routes/documentroute.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const documentController = require("../controllers/documentController");
 const upload = require('../middlewares/uploadMiddleware')
 const authMiddleware = require('../middlewares/authMiddleware')
+const audit = require("../middlewares/historyMiddleware")
 
 
 // Routes CRUD
 
 // POST /documents → créer un document (upload d'un fichier)
-router.post("/",authMiddleware, (req, res, next) => {
+router.post("/",authMiddleware,audit("document_cree"), (req, res, next) => {
     upload.single("file")(req, res, (err) => {
       if (err) {return res.status(400).json({ message: err.message })}
       next();
@@ -19,12 +20,12 @@ router.post("/",authMiddleware, (req, res, next) => {
 router.get("/",authMiddleware, documentController.getDocuments);
 
 // GET /documents/:id → récupérer un document par ID
-router.get("/:id",authMiddleware, documentController.getDocumentById);
+router.get("/:id",authMiddleware, audit("get_document_by_id"), documentController.getDocumentById);
 
 // DELETE /documents/:id → supprimer un document
-router.delete("/:id",authMiddleware, documentController.deleteDocument);
+router.delete("/:id",authMiddleware, audit("delete_document_by_id"), documentController.deleteDocument);
 
 // PUT /documents/:id/rename → renommer un fichier 
-router.put("/:id/rename", documentController.renameDocument);
+router.put("/:id/rename",authMiddleware, audit("rename_document"), documentController.renameDocument);
 
 module.exports = router;
