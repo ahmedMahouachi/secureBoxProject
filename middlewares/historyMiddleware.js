2bd44e7da9cd6d36669f83b71a7bf31ce578ffdf
Manon <vatresmanon17@gmail.com>
2025-08-14 09:51:57 +0200

    add middleware and update user

---
+++ middlewares/historyMiddleware.js
@@ -0,0 +1,31 @@
+const History = require("../models/History");
+
+const createHistoryMiddleware = async (req, res, next) => {
+    try {
+        //Soit dans le token soit dans le params de la req
+        const historyUserId = req.user?.id || req.params.id
+        const historyRoute = req.originalUrl; // par défaut l'URL appelée
+        const historyAction = req.body?.action || "GET";
+        const historyMethod = req.method;
+        const historyAdresseIP =
+            req.headers["x-forwarded-for"]?.split(",")[0] ||
+            req.socket.remoteAddress ||
+            "0.0.0.0";
+
+        await History.create({
+            userId: historyUserId,
+            action: historyAction,
+            route: historyRoute,
+            method: historyMethod,
+            adresseIP: historyAdresseIP
+        });
+
+        // On continue avec la suite de la requête
+        next();
+    } catch (error) {
+        console.error("Erreur lors de la création de l'historique :", error.message);
+        next(error);
+    }
+};
+
+module.exports = createHistoryMiddleware;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
+const createHistoryMiddleware = require("../middlewares/historyMiddleware")
 const {
     getHistory,
     getHistoryById,
-    createHistoryById,
+//    createHistoryById,
     deleteHistory,
     getAllUser,
-    deleteUserById
+    deleteUserById,
+    updateHistory
 } = require("../controllers/adminController");
 
 const router = express.Router();
 
 //Route de l'historique 
-router.get("/get_history/:id", getHistory);
-router.get("/get_history_by_id/:historyId", getHistoryById);
+router.get("/get_history/:id",createHistoryMiddleware, getHistory);
+router.get("/get_history_by_id/:historyId",authMiddleware, createHistoryMiddleware, getHistoryById);
 
-router.post("/create_history",authMiddleware, createHistoryById);
-router.delete("/delete_history/:historyId", deleteHistory);
+//router.post("/create_history",authMiddleware,createHistoryMiddleware, createHistoryById);
+router.delete("/delete_history/:historyId",authMiddleware, createHistoryMiddleware, deleteHistory);
+router.put("/update_history/:historyId",authMiddleware, createHistoryMiddleware, updateHistory);
 
 //Route User
 router.get("/get_all_user",getAllUser);
-router.delete("/delete_user_by_id",authMiddleware, deleteUserById);
-//router.put("/update_history/:historyId", updateHistory);
+router.delete("/delete_user_by_id", deleteUserById);
 
 
 module.exports = router;
